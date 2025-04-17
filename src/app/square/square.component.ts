e3486a9db4601e01557bcf0eab4b4ae20eeb9b96
rahmanhamim <rahmanhamiminfo@gmail.com>
2025-04-17 21:11:54 +0600

    updated square

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,10 @@
+import { Component, NgZone, ChangeDetectorRef, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss'],
+})
+export class SquareComponent {
+  @Input() value?: 'X' | 'O';
+}
