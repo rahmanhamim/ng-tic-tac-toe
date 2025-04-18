14b6f25b571f220a9efbcf7cd0989aed8b1bff5c
rahmanhamim <rahmanhamiminfo@gmail.com>
2025-04-18 21:28:32 +0600

    updated tic tac toe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SquareComponent } from './square/square.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SquareComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'first-ng-app';
+  board: string[] = Array(9).fill('');
+  currentPlayer: string = 'X';
+  winner: string | null = null;
+
+  makeMove(index: number) {
+    if (!this.board[index] && !this.winner) {
+      this.board[index] = this.currentPlayer;
+      if (this.checkWinner()) {
+        this.winner = this.currentPlayer;
+      } else {
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      }
+    }
+  }
+
+  checkWinner(): boolean {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return winningCombinations.some((combination) => {
+      const [a, b, c] = combination;
+      return (
+        this.board[a] &&
+        this.board[a] === this.board[b] &&
+        this.board[a] === this.board[c]
+      );
+    });
+  }
+
+  resetGame() {
+    this.board = Array(9).fill('');
+    this.currentPlayer = 'X';
+    this.winner = null;
+  }
 }
